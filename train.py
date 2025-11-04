c835cfe13cf9a8f19fb34c10aea6eecbcfb265b5
ioarun <arun_kumar271@hotmail.com>
2025-11-05 00:09:02 +1100

    Add tokenizer

--- train.py
+++ train.py
@@ -3,3 +3,24 @@ import torch.nn as nn
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+from pathlib import Path
+
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item['translation'][lang]
+
+def get_or_build_tokenizer(config, ds, lang):
+    tokenizer_path = Path(config['tokenizer_file']).format(lang)
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[BOS]", "[EOS]"], min_frequency=config['min_token_freq'])
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(tokenizer_path)
+    return tokenizer
+
